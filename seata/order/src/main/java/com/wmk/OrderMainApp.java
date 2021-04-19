d721ee3c044f1e8e8b922874a6a30d8b1a7b122d
wmk <915672375@qq.com>
2021-04-19 13:00:05 +0800

    基于seata分布式事务的框架搭建

---
+++ seata/order/src/main/java/com/wmk/OrderMainApp.java
@@ -0,0 +1,16 @@
+package com.wmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @USER: WangMeiKai
+ * @DATE: 2021/4/19
+ * @TIME: 11:32
+ **/
+@SpringBootApplication
+public class OrderMainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainApp.class,args);
+    }
+}
---
+++ seata/product/src/main/java/com/wmk/ProductMainApp.java
@@ -0,0 +1,16 @@
+package com.wmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @USER: WangMeiKai
+ * @DATE: 2021/4/19
+ * @TIME: 11:33
+ **/
+@SpringBootApplication
+public class ProductMainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMainApp.class,args);
+    }
+}
