1a1870ae8e07d1270c3760e870b5d530b352c1d1
wmk <915672375@qq.com>
2021-04-30 18:14:36 +0800

    注意claim和过期时间的顺序，若颠倒会使得过期时间被覆盖，导致过期时间失效，

--- spring-cloud-oauth2/jwtTest/src/main/java/JwtUtils.java
+++ spring-cloud-oauth2/jwtTest/src/main/java/JwtUtils.java
@@ -1,43 +1,47 @@
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 public class JwtUtils {
 
     static String token;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         generateJwt();
+        TimeUnit.SECONDS.sleep(10);
         testParseJwt();
     }
 
     public static void generateJwt() {
-        JwtBuilder builder= Jwts.builder()
+
+        Map<String, Object> claim = new HashMap<>();
+        claim.put("name", "wmk");
+        claim.put("age", "100");
+        claim.put("school", "sc");
+
+        JwtBuilder builder = Jwts.builder()
                 .setId("110")             //设置唯一编号
                 .setSubject("牛逼")       //设置主题  可以是JSON数据
                 .setIssuedAt(new Date())  //设置签发日期
-                .signWith(SignatureAlgorithm.HS256,"abcd");//设置签名 使用HS256算法，并设置SecretKey(字符串)
-        Map<String,Object> claim = new HashMap<>();
-        claim.put("name","wmk");
-        claim.put("age","100");
-        claim.put("school","sc");
-        builder.setClaims(claim);
+                .setClaims(claim)           //注意先后顺序
+                .setExpiration(new Date(System.currentTimeMillis()+1000 * 10))  //注意先后顺序
+                .signWith(SignatureAlgorithm.HS256, "wqeqwewqeqwewq");//设置签名 使用HS256算法，并设置SecretKey(字符串)
+
         //构建 并返回一个字符串
         token = builder.compact();
         System.out.println(token);
     }
 
-    public static void testParseJwt(){
-        Claims claims = Jwts.parser().
-                setSigningKey("abcd").
-                parseClaimsJws(token).
-                getBody();
+    public static void testParseJwt() {
+        Claims claims = Jwts.parser()
+                .setSigningKey("wqeqwewqeqwewq")
+                .parseClaimsJws(token)
+                .getBody();
+
         System.out.println(claims);
     }
 
