5966e2674531fe61fb01978d33cb307d92b6db90
wmk <915672375@qq.com>
2021-05-29 17:04:50 +0800

    新建rocketMQ模块

---
+++ rocketmq/provider/src/main/java/com/wmk/controller/ConsumerController.java
@@ -0,0 +1,27 @@
+package com.wmk.controller;
+
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+
+@RestController
+public class ConsumerController {
+
+    @Value("${tl.rocketmq.topic}")
+    private String springTopic;
+
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
+
+    @GetMapping("consumer")
+    public String index(){
+        SendResult result = rocketMQTemplate.syncSend(springTopic,"发送消息");
+        return "send_status:"+result.getSendStatus().name();
+    }
+
+}
