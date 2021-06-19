692ebae18aa862f192bda2cdf222dad04721498a
wmk <915672375@qq.com>
2021-06-19 17:32:49 +0800

    添加tcc的主类

---
+++ distributedTransaction/tcc/tccbank1/src/main/java/com/wmk/tccbank1MainApp.java
@@ -0,0 +1,11 @@
+package com.wmk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tccbank1MainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(tccbank1MainApp.class,args);
+    }
+}
